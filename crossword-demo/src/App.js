06f3c92188eb5a292ef556fc76be497c2c08ba2f
mitosagi <54105954+mitosagi@users.noreply.github.com>
2020-09-08 21:39:11 +0900

    Migrate to create-react-app

--- crossword-demo/src/App.js
+++ crossword-demo/src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { genarate } from './gen';
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Cell extends Component {
+  render() {
+    const text = this.props.text
+    let render_text = text
+    if (text === "-") {
+      render_text = '■'
+    } else if (this.props.answer[text] === "") {
+      render_text = '　'
+    } else {
+      render_text = this.props.answer[text]
+    }
+    return (
+      <span className={"Table" + ((text === this.props.selected) ? " selected" : "")} onClick={() => this.props.clicked(text)}>{render_text}</span>
+    );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: [], answer: {}, selected: "" };
+    /*
+  盤面を生成
+  行をmapしてから各要素をmapすることでとりあえず盤面を表示する
+*/
+  }
+
+  async componentDidMount() {
+    const arr = await genarate(12)
+    const char_set = new Set(arr.reduce((a, b) => a.concat(b)))
+    char_set.delete("-")
+    const ans = {}
+    char_set.forEach(a => ans[a] = a)
+    ans[Object.keys(ans)[0]] = ""
+    console.log(ans)
+    this.setState({ data: arr, answer: ans });
+  }
+
+  clicked = (text) => {
+    if (text === "-") return
+    this.setState({ selected: text })
+  }
+
+  onKeyPress = (button) => {
+    console.log("Button pressed", button);
+  }
+
+  render() {
+    const kana50 = [
+      "んわらやまぱばはなだたざさがかあ",
+      "　ゐり　みぴびひにぢちじしぎきい",
+      "　　るゆむぷぶふぬづつずすぐくう",
+      "　ゑれ　めぺべへねでてぜせげけえ",
+      "　をろよもぽぼほのどとぞそごこお"
+    ].map(row => row.split("").join(" ")).concat(["{space}"])
+    return (
+      <div className="App">
+        {this.state.data.map((row, i) =>
+          <div key={i}>
+            {row.map((square, j) =>
+              <Cell key={j} text={square} answer={this.state.answer} clicked={this.clicked} selected={this.state.selected}></Cell>
+            )}
+          </div>
+        )}
+        <div className="keyboard">
+          <Keyboard onKeyPress={input => this.onKeyPress(input)} layout={{ default: kana50 }}></Keyboard>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ crossword-demo/src/gen.js
@@ -0,0 +1,91 @@
+import clg from 'crossword-layout-generator'
+
+let isInitialized = false
+let words
+let char_dict
+
+async function genarate(char_count) {
+    if (isInitialized) return Promise.resolve(generate_crossword(false, char_count, words, char_dict))
+    isInitialized = true
+
+    function load(text) {
+        const lines = text.toString().split('\n').slice(8);
+
+        // [Regex Tester - Javascript, PCRE, PHP](https://www.regexpal.com/)
+        const lines2 = lines.map(line => line.split('\t')[0])
+            .filter(word => word.match(/^[ぁ-んー]+$/))
+            .filter(word => !word.match(/^([ぁ-んー]{1,2})\1$/))
+            .filter(word => !word.match(/^([ぁ-んー]{1})\1\1$/))
+            .filter(word => !word.match(/くん$/))
+            .filter(word => word.length <= 8)
+            .filter(word => word.length >= 4)
+        const valid_words = Array.from(new Set(lines2))
+
+        const characters = valid_words.reduce((a, b) => a + b)
+        const note = {}
+        for (let i = 0; i < characters.length; i++) {
+            const char = characters[i]
+            let char_count = note[char] == null ? 1 : note[char] + 1
+            note[char] = char_count
+        }
+        return [valid_words, note]
+    }
+
+    return fetch('dic-nico-intersection-pixiv.txt')
+        .then(async response => [words, char_dict] = load(await response.text()))
+        .then(() => generate_crossword(false, char_count, words, char_dict))
+}
+
+function generate_crossword(isConsole, char_count, words, char_dict) {
+    function choose_n(arr, num) {
+        function choose(arr) {
+            const total = Object.values(arr).reduce((a, b) => a + b)
+            const arrow = Math.random() * total;
+            let threshold = 0.0;
+            for (const [key, value] of Object.entries(arr)) {
+                threshold += value
+                if (arrow < threshold) { return key }
+            }
+        }
+
+        const ret_arr = []
+        for (let i = 0; i < num; i++) {
+            const choosed = choose(arr)
+            delete arr[choosed]
+            ret_arr.push(choosed)
+        }
+
+        return ret_arr
+    }
+
+    const n_char = choose_n(char_dict, char_count)
+
+    const filtered_words = words.filter(word => word.split("").map(c => n_char.includes(c)).reduce((a, b) => a && b))
+
+    // visualize
+
+    function convert_to_json(word_list) {
+        var json_data = [];
+        for (let i in word_list) {
+            if (word_list[i].length > 0) {
+                json_data[i] = { "answer": word_list[i].toLowerCase() }
+            }
+        }
+
+        return json_data;
+    }
+
+    const input_json = convert_to_json(filtered_words)
+
+    // Output data
+    const layout = clg.generateLayout(input_json)
+
+    if (isConsole) {
+        const output_html = layout.table_string.replace(/-/g, '⬛').replace(/<br>/g, '\n')
+        console.log(output_html)
+    } else {
+        return layout.table
+    }
+}
+
+export { genarate }
