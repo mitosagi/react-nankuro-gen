4610989f5b58c2ba3f942911fb3a69dc3057a972
mitosagi <54105954+mitosagi@users.noreply.github.com>
2021-04-03 11:55:33 +0900

    Updates

---
+++ precache-manifest.4b9428c484a962d1fa446a492d8affaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c73a271f1be32a954b6516952d12d4",
+    "url": "/react-nankuro-gen/index.html"
+  },
+  {
+    "revision": "db46bd6e73ada780568d",
+    "url": "/react-nankuro-gen/static/css/2.667d6919.chunk.css"
+  },
+  {
+    "revision": "dbbc021db51c468d27d0",
+    "url": "/react-nankuro-gen/static/css/main.a1dfc08a.chunk.css"
+  },
+  {
+    "revision": "db46bd6e73ada780568d",
+    "url": "/react-nankuro-gen/static/js/2.44f1b12a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-nankuro-gen/static/js/2.44f1b12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbc021db51c468d27d0",
+    "url": "/react-nankuro-gen/static/js/main.bc6a2e0b.chunk.js"
+  },
+  {
+    "revision": "b0a24b3a7eb6111fa525",
+    "url": "/react-nankuro-gen/static/js/runtime-main.3e12d30f.js"
+  }
+]);
