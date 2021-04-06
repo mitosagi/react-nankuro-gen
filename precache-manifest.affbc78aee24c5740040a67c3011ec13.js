69a0ad4f948d2fd3b33c149e4594daa9d100474f
mitosagi <54105954+mitosagi@users.noreply.github.com>
2021-04-06 22:08:59 +0900

    Updates

---
+++ precache-manifest.affbc78aee24c5740040a67c3011ec13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b656fe1c19f5584db07c0c3b866fb74d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db46bd6e73ada780568d",
+    "url": "./static/css/2.667d6919.chunk.css"
+  },
+  {
+    "revision": "526e32400d6e8d414b89",
+    "url": "./static/css/main.a1dfc08a.chunk.css"
+  },
+  {
+    "revision": "db46bd6e73ada780568d",
+    "url": "./static/js/2.44f1b12a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.44f1b12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526e32400d6e8d414b89",
+    "url": "./static/js/main.8f88da1d.chunk.js"
+  },
+  {
+    "revision": "a67ec072aa78005177ab",
+    "url": "./static/js/runtime-main.8cc11f8c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-nankuro-gen/precache-manifest.4b9428c484a962d1fa446a492d8affaa.js"
+  "./precache-manifest.affbc78aee24c5740040a67c3011ec13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-nankuro-gen/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
